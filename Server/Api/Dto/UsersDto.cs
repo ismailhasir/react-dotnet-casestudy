fbf66dcbb4f07b9c6617d0f70e7692c09f3c2f00
Muhammet Bozkaya <muhammetbozkaya20@gmail.com>
2022-01-08 18:11:03 +0300

    server implementations

---
+++ Server/Api/Dto/UsersDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Dto
+{
+    public class UsersDto
+    {
+        public class LoginRequest
+        {
+            public string Email { get; set; }
+            public string Password { get; set; }
+        }
+
+        public class RegisterRequest
+        {
+            public string Email { get; set; }
+            public string Password { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+
+        }
+    }
+}
---
+++ Server/Api/Model/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Model
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
