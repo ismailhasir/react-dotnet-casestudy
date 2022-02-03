6632e064d3d313cc4829c4e5cb20a29b07531994
hakanozcan <hakanozcan20@hotmail.com.tr>
2022-02-04 00:19:07 +0300

    migrations added

--- Server/Api/Model/ApiDbContext.cs
+++ Server/Api/Model/ApiDbContext.cs
@@ -13,9 +13,24 @@ namespace Api.Model
         {
 
         }
+
         public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Role> Roles { get; set; }
+        public virtual DbSet<Class> Classes { get; set; }
+        public virtual DbSet<StudentClass> StudentClasses { get; set; }
+        public virtual DbSet<TeacherClass> TeacherClasses { get; set; }
 
         partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Role>().HasData(
+                new Role { Id = 1, Title = "Admin" },
+                new Role { Id = 2, Title = "Teacher" },
+                new Role { Id = 3, Title = "Student" },
+                new Role { Id = 4, Title = "Parent" }
+                
+            );
+        }
     }
 }
---
+++ Server/Api/Model/TeacherClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Model
+{
+    public class TeacherClass
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int ClassId { get; set; }
+
+        public int TeacherId { get; set; }
+
+        public virtual Class Classes { get; set; }
+        public virtual User User { get; set; }
+    }
+}
